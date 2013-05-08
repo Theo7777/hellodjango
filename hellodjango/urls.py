ece05758fb958561ab444ada06ed7518eac40f38
Theo <theo.ohene@gmail.com>
2013-05-08 10:10:50 +0100

    added django.conf settings

--- hellodjango/urls.py
+++ hellodjango/urls.py
@@ -1,6 +1,6 @@
 from django.conf.urls import patterns, include, url
 from writer.views import EmailInput
-
+from django.conf import settings
 
 # Uncomment the next two lines to enable the admin:
 from django.contrib import admin
