9b4e6f9dad266a807787ebf914ab74ede0598bfa
Theo <theo.ohene@gmail.com>
2013-05-08 09:56:40 +0100

    added static

--- hellodjango/settings.py
+++ hellodjango/settings.py
@@ -78,18 +78,17 @@ import os.path
 # Example: "http://example.com/static/", "http://static.example.com/"
 STATIC_URL = '/static/'
 
-# Additional locations of static files
-# STATICFILES_DIRS = (
-#       os.path.join(PROJECT_PATH, 'writer/static'),
-# )
-
+Additional locations of static files
 STATICFILES_DIRS = (
-    "/Users/Theo/hellodjango/writer/static"
-    )
-    # Put strings here, like "/home/html/static" or "C:/www/django/static".
-    # Always use forward slashes, even on Windows.
-    # Don't forget to use absolute paths, not relative paths.
+      os.path.join(PROJECT_PATH, 'writer/static'),
 )
+# 
+# STATICFILES_DIRS = (
+   "/Users/Theo/hellodjango/writer/static"
+#     )
+# Put strings here, like "/home/html/static" or "C:/www/django/static".
+# Always use forward slashes, even on Windows.
+# Don't forget to use absolute paths, not relative paths.
 
 # List of finder classes that know how to find static files in
 # various locations.
