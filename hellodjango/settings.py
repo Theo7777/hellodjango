7a816411fadfe41b79ad15d57576a24fe6d38454
Theo <theo.ohene@gmail.com>
2013-05-08 09:35:02 +0100

    changed static dir

--- hellodjango/settings.py
+++ hellodjango/settings.py
@@ -72,13 +72,16 @@ MEDIA_URL = ''
 # Example: "/var/www/example.com/static/"
 STATIC_ROOT = ''
 
+ADMIN_MEDIA_PREFIX = '/static/admin/'
+
 # URL prefix for static files.
 # Example: "http://example.com/static/", "http://static.example.com/"
 STATIC_URL = '/static/'
 
 # Additional locations of static files
 STATICFILES_DIRS = (
-   "/Users/Theo/hellodjango/writer/static/",
+      os.path.join(PROJECT_PATH, 'writer/static'),
+)
     # Put strings here, like "/home/html/static" or "C:/www/django/static".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
--- hellodjango/urls.py
+++ hellodjango/urls.py
@@ -1,5 +1,6 @@
 from django.conf.urls import patterns, include, url
 from writer.views import EmailInput
+from django.conf import settings
 
 # Uncomment the next two lines to enable the admin:
 from django.contrib import admin
@@ -17,4 +18,9 @@ urlpatterns = patterns('',
 
     # Uncomment the next line to enable the admin:
     url(r'^admin/', include(admin.site.urls)),
+
+    if not settings.DEBUG:
+    urlpatterns += patterns('',
+        (r'^static/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.STATIC_ROOT}),
+    )
 )
